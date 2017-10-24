e429574b797eb9e03232f9d1941895e67d59c496
azarateo <azarateo@gmail.com>
2017-10-24 12:44:42 -0500

    Exercise adding 4 functions

--- app.js
+++ app.js
@@ -17,10 +17,13 @@ if(command === 'add'){
   notes.addNote(argvector.title,argvector.body);
 }else if(command === 'list'){
   console.log('Listing all notes');
+  notes.getAll();
 }else if(command === 'read'){
   console.log('Fetching specific note');
+  notes.readNote();
 }else if(command === 'remove'){
   console.log('Removing note');
+  notes.removeNote();
 }
 else{
   console.log("The argument is not a known command. Try 'add', 'list', 'read' or 'remove'");
--- notes.js
+++ notes.js
@@ -3,7 +3,18 @@ console.log('Strating notes.js');
 var addNote = (title,body) => {
   console.log('Adding note with title:',title,' and body:',body);
 };
-
+var getAll = () => {
+  console.log('Getting all notes');
+};
+var readNote = (title) => {
+  console.log('Getting note with title:',title);
+};
+var removeNote = (title) => {
+  console.log('Removing note with title:',title);
+};
 module.exports = {
-  addNote
+  addNote,
+  getAll,
+  readNote,
+  removeNote
 };
