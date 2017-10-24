9765b91573114bf8447499cebdd48d3b8f5235bb
azarateo <azarateo@gmail.com>
2017-10-24 10:13:11 -0500

    Section 3 Lecture 13 end

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ const fs = require('fs');
 const _ = require('lodash');
 //My modules
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command: '+command);
+console.log(process.argv);
+
+if(command === 'add'){
+  console.log('Adding new note');
+}else if(command === 'list'){
+  console.log('Listing all notes');
+}else if(command === 'read'){
+  console.log('Fetching specific note');
+}else if(command === 'remove'){
+  console.log('Removing note');
+}
+else{
+  console.log("The argument is not a known command. Try 'add', 'list', 'read' or 'remove'");
+}
