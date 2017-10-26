87bec6cf7f8f59d7c2b06d168ad097be52ab1b9f
azarateo <azarateo@gmail.com>
2017-10-25 21:15:00 -0500

    String to object with parse

--- playground/json.js
+++ playground/json.js
@@ -1,6 +1,11 @@
-var obj = {
-  name: 'Andrew'
-};
+// var obj = {
+//   name: 'Andrew'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(stringObj);
 
-var stringObj = JSON.stringify(obj);
-console.log(stringObj);
+var personString = '{"name":"Alejandro","age":39}';
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
