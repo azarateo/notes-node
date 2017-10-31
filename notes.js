8ddf06a0dc8c987e3ade42d1972f3f7fdf99a6e9
azarateo <azarateo@gmail.com>
2017-10-31 11:28:05 -0500

    first attempt getall() Section 3 Lecture 17

--- notes.js
+++ notes.js
@@ -3,34 +3,33 @@ const fs = require('fs');
 
 var addNote = (title,body) => {
   console.log('Adding note with title:',title,' and body:',body);
-
-  var notes = [];
+  var notes = getAll();
   var note = {
     title: title,
     body: body
   };
-
-  try{
-      var notesString = fs.readFileSync('notesData.json');
-      notes = JSON.parse(notesString);
-  }catch(err){
-      console.log('Creating first note');
-  }finally{
-    var duplicates = notes.filter((element)=>{return element.title === title});
-
-    if(duplicates.length===0){
-      notes.push(note);
-      fs.writeFileSync('notesData.json',JSON.stringify(notes));
-    }else{
-      console.log('A note with that title already exists');
-    }
+  var duplicates = notes.filter((element)=>{return element.title === title});
+  if(duplicates.length===0){
+    notes.push(note);
+    fs.writeFileSync('notesData.json',JSON.stringify(notes));
+  }else{
+    console.log('A note with that title already exists');
   }
-
-
 };
+
 var getAll = () => {
   console.log('Getting all notes');
+  try{
+    var notesString = fs.readFileSync('notesData.json');
+    notes = JSON.parse(notesString);
+  }catch(err){
+    console.log('notesData.json was not opened');
+    console.log(err);
+    console.log('Creating first note');
+  }
+  return notes;
 };
+
 var readNote = (title) => {
   console.log('Getting note with title:',title);
 };
