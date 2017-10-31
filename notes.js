8a4fe739c0844477dc3b17096a63a98704501526
azarateo <azarateo@gmail.com>
2017-10-31 11:59:12 -0500

    fetchNotes and saveNotes

--- notes.js
+++ notes.js
@@ -1,24 +1,8 @@
 console.log('Strating notes.js');
 const fs = require('fs');
 
-var addNote = (title,body) => {
-  console.log('Adding note with title:',title,' and body:',body);
-  var notes = getAll();
-  var note = {
-    title: title,
-    body: body
-  };
-  var duplicates = notes.filter((element)=>{return element.title === title});
-  if(duplicates.length===0){
-    notes.push(note);
-    fs.writeFileSync('notesData.json',JSON.stringify(notes));
-  }else{
-    console.log('A note with that title already exists');
-  }
-};
-
-var getAll = () => {
-  console.log('Getting all notes');
+var fetchNotes = () => {
+  var notes = [];
   try{
     var notesString = fs.readFileSync('notesData.json');
     notes = JSON.parse(notesString);
@@ -28,6 +12,31 @@ var getAll = () => {
     console.log('Creating first note');
   }
   return notes;
+}
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notesData.json',JSON.stringify(notes));
+}
+
+var addNote = (title,body) => {
+  console.log('Adding note with title:',title,' and body:',body);
+  var notes = fetchNotes();
+  var note = {
+    title: title,
+    body: body
+  };
+  var duplicates = notes.filter((element)=>{return element.title === title});
+  if(duplicates.length===0){
+    notes.push(note);
+    saveNotes(notes);
+  }else{
+    console.log('A note with that title already exists');
+  }
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+
 };
 
 var readNote = (title) => {
