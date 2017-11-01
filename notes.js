e6599bfe8ee1a78ac4fb57272ee6e633e16ab289
azarateo <azarateo@gmail.com>
2017-11-01 09:37:42 -0500

    Terminé de hacer comentarios
    Comentarios descriptivos de las funciones.

--- notes.js
+++ notes.js
@@ -1,6 +1,6 @@
 console.log('Strating notes.js');
 const fs = require('fs');
-
+// fetchNotes Method
 var fetchNotes = () => {
   var notes = [];
   try{
@@ -13,11 +13,11 @@ var fetchNotes = () => {
   }
   return notes;
 }
-
+// saveNotes method
 var saveNotes = (notes) => {
   fs.writeFileSync('notesData.json',JSON.stringify(notes));
 }
-
+// addNote method
 var addNote = (title,body) => {
   console.log('Adding note with title:',title,' and body:',body);
   var notes = fetchNotes();
@@ -30,21 +30,24 @@ var addNote = (title,body) => {
     notes.push(note);
     saveNotes(notes);
   }else{
-    console.log('A note with that title already exists');
+    console.log('Title already in use. No note was added');
   }
 };
-
+// getAll method
 var getAll = () => {
   console.log('Getting all notes');
 
 };
-
+// readNote method
 var readNote = (title) => {
   console.log('Getting note with title:',title);
 };
+// removeNote method
 var removeNote = (title) => {
   console.log('Removing note with title:',title);
 };
+
+//file interface, available methods or exports
 module.exports = {
   addNote,
   getAll,
